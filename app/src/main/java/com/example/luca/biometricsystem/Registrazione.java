389b877ecf196a5bb8b8539e9bfcece864a25476
ilSabba <lucasabatini1996@gmail.com>
2019-09-25 18:56:54 +0200

    registrazione2.0

--- app/src/main/java/com/example/luca/biometricsystem/Registrazione.java
+++ app/src/main/java/com/example/luca/biometricsystem/Registrazione.java
@@ -8,15 +8,26 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputLayout;
+
 
 public class Registrazione extends AppCompatActivity {
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
     private FloatingActionButton cameraButton;
     private ImageView userImage;
+    private TextInputLayout firstName;
+    private TextInputLayout lastName;
+    private TextInputLayout studentId;
+    private String firstNameValue;
+    private String lastNameValue;
+    private String studentIdValue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +36,70 @@ public class Registrazione extends AppCompatActivity {
 
         cameraButton = findViewById(R.id.camera_button);
         userImage = findViewById(R.id.user_image);
+        firstName = findViewById(R.id.first_name_layout);
+        lastName = findViewById(R.id.last_name_layout);
+        studentId = findViewById(R.id.student_id_layout);
+    }
+
+
+    public boolean validateLastName(){
+        lastNameValue = lastName.getEditText().getText().toString().trim();
+
+        if (lastNameValue.isEmpty()){
+            lastName.setError("Field can't be empty");
+            return false;
+        }else{
+            lastName.setError(null);
+            return true;
+        }
+    }
+
+    public boolean validateStudentId(){
+        studentIdValue = studentId.getEditText().getText().toString().trim();
+
+        if (studentIdValue.isEmpty()){
+            studentId.setError("Field can't be empty");
+            return false;
+        }else{
+            studentId.setError(null);
+            return true;
+        }
+    }
+
+    public boolean validateFirstName(){
+        firstNameValue = firstName.getEditText().getText().toString().trim();
+
+        if (firstNameValue.isEmpty()){
+            firstName.setError("Field can't be empty");
+            return false;
+        }else{
+            firstName.setError(null);
+            return true;
+        }
     }
 
     public void openCamera(View view){
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, 0);
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    public void registraValori(View view){
+        if( !validateFirstName() | !validateLastName() | !validateStudentId()){
+            return;
+        }
+
+        Toast.makeText(this, "OK", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-        userImage.setImageBitmap(bitmap);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            userImage.setImageBitmap(imageBitmap);
+        }
     }
 }
