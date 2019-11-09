ddf0e266efe8044fa5e7d8aa0d0f16cbbe36e71b
ilSabba <lucasabatini1996@gmail.com>
2019-11-09 20:00:54 +0100

    SplashScreen done

---
+++ app/src/main/java/com/example/luca/biometricsystem/SplashScreen.java
@@ -0,0 +1,21 @@
+package com.example.luca.biometricsystem;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.luca.biometricsystem.logingeneroso.LoginActivity;
+import com.example.luca.biometricsystem.logingeneroso.LoginIntroFragment;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
