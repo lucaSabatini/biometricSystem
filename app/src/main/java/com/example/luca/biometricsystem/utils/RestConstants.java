d388c922e7508bbcc82321daf333da8c460c6063
LAPTOP-1DLKJ958\franc <francesco.sassi.phys@gmail.com>
2019-11-19 12:06:40 +0100

    server api inserted

--- app/src/main/java/com/example/luca/biometricsystem/utils/RestConstants.java
+++ app/src/main/java/com/example/luca/biometricsystem/utils/RestConstants.java
@@ -1,5 +1,23 @@
 package com.example.luca.biometricsystem.utils;
 
 public class RestConstants {
-    public static final String baseUrl = "";
+    private static final String BASEURL = "https://appello-project.appspot.com";
+    private static final String COURSE = BASEURL + "/courses";
+
+    public static String getCourseUrl(String accessToken, String uid){
+        return String.format("%s/getCourse?accessToken=%s&uid%s", COURSE, accessToken, uid);
+    }
+
+    public static String postCourseUrl(String accessToken, String uid){
+        return String.format("%s/postCourse?accessToken=%s&uid%s", COURSE, accessToken, uid);
+    }
+
+    public static String deleteCourseUrl(String accessToken, String uid){
+        return String.format("%s/deleteCourse?accessToken=%s&uid%s", COURSE, accessToken, uid);
+    }
+
+    public static String getAllCoursesByIdUrl(String accessToken, String uid){
+        return String.format("%s/getAllCoursesById?accessToken=%s&uid%s", COURSE, accessToken, uid);
+    }
+
 }
